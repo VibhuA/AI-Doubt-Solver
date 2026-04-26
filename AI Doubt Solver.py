9ef5e1c19f892cace79e533d5089e8002a85f6b4
VibhuA <68466801+VibhuA@users.noreply.github.com>
2026-04-26 13:09:25 +0530

    Update AI Doubt Solver.py

--- AI Doubt Solver.py
+++ AI Doubt Solver.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 # 1. AI SETUP (Gemini 2.5 Flash)
 # ==========================================
 # Replace with your actual key from Google AI Studio
-genai.configure(api_key="YOUR_API_KEY_HERE")
+genai.configure(api_key="AIzaSyBmQ5hVHo2WZdK2SUvrUvHW-YNEMkDguEk")
 
 MATH_SYSTEM_PROMPT = """
 You are the 'HFS Math Specialist', an elite tutor for ICSE Class 8 and 9. 
